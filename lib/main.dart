821983f1d4095b5f310c169f57a90d2945466225
Johnkaramw <johnmaram400@gmail.com>
2025-06-04 00:52:28 +0300

    finsh app

--- lib/main.dart
+++ lib/main.dart
@@ -4,9 +4,9 @@ import 'package:newsss_appp/views/home_page.dart';
 void main() {
   runApp(NewsApp());
 }
+
 class NewsApp extends StatelessWidget {
   const NewsApp({super.key});
-
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
